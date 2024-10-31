08f1d41f0d9744f6c1b16207ed7c48d89a201a51
孙冬航 <sundonghang@wdtrip.com>
2024-10-31 14:00:29 +0800

    latent

--- nodes.py
+++ nodes.py
@@ -133,7 +133,7 @@ class SinglePortraitNode(StoryMakerBaseNode):
             noise_mask = comfy.sample.prepare_mask(noise_mask, noise.shape, device)
 
         noise = noise.to(device)
-        latent_image = latent_image.to(torch.float32)
+        latent_image = latent_image.to(torch.float64)
 
         generator = torch.Generator(device='cuda').manual_seed(seed)
         output = self.shared.pipe(
